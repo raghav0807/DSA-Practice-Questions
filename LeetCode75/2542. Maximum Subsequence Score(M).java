6d795c695c70071018130f52cff1d55badb6e502
raghav0807 <raghav.agiwal2002@gmail.com>
2024-08-10 14:03:53 +0530

    2462. Total Cost to Hire K Workers(M)  {Array,Greedy,Sorting,Heap}

---
+++ LeetCode75/2542. Maximum Subsequence Score(M).java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length;
+        Integer[] index = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            index[i] = i;
+        }
+        
+        Arrays.sort(index, (a, b) -> nums2[b] - nums2[a]);
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k);
+        long sum = 0;
+        long result = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int idx = index[i];
+            sum += nums1[idx];
+            pq.offer(nums1[idx]);
+            
+            if (pq.size() > k) {
+                sum -= pq.poll();
+            }
+            
+            if (pq.size() == k) {
+                result = Math.max(result, sum * nums2[idx]);
+            }
+        }
+        
+        return result;
+    }
+}
+
+// 2542. Maximum Subsequence Score(M)
